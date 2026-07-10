01e5ef9f925449928ea775fdfbba03e4a3dba6db
Sherwin Dave Osma <sherwinosma1@gmail.com>
2026-07-10 11:36:15 +0800

    Gantt Chart custom painter widget

--- schedulerwidget.h
+++ schedulerwidget.h
@@ -8,3 +8,23 @@
 #include <QTableWidget>
 #include <QScrollArea>
 #include "processscheduler.h"
+
+// Gantt Chart custom painter widget
+class GanttWidget : public QWidget {
+    Q_OBJECT
+public:
+    explicit GanttWidget(QWidget *parent = nullptr);
+
+    void setGanttData(const QList<GanttEntry> &entries, int totalTicks, int currentTick);
+    void setCurrentTick();
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+    void resizeEvent(QResizeEvent *event) override;
+
+private:
+    QList<GanttEntry> m_entries;
+    int m_totalTicks = 0;
+    int m_currentTick = 0;
+    int m_rowH = 36;
+};
