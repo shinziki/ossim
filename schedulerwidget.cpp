96d0a63486300972775c34ed51d96227f7d620bb
Sherwin Dave Osma <sherwinosma1@gmail.com>
2026-07-17 07:15:37 +0800

    Pallete

--- schedulerwidget.cpp
+++ schedulerwidget.cpp
@@ -9,5 +9,15 @@
 #include <QFrame>
 #include <QSplitter>
 #include <QScrollBar>
+#include <QFont>
+#include <QFontMetrics>
+#include <QRandomGenerator>
+#include <cmath>
+
+// Pallete
+const QVector<QColor> SchedulerWidget::PROC_COLORS = {
+    QColor("#00e5ff"), QColor("#39ff14"), QColor("#ff9f0a"),
+    QColor("#bf5af2"), QColor("#ff2d55"), QColor("#30d158")
+};
 
 schedulerwidget::schedulerwidget() {}
