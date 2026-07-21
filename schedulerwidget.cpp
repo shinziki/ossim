4602f5f315ab7e8f883244af554cf8f11f5d2198
Sherwin Dave Osma <sherwinosma1@gmail.com>
2026-07-21 08:54:57 +0800

    GanttWidget

--- schedulerwidget.cpp
+++ schedulerwidget.cpp
@@ -17,7 +17,20 @@
 // Pallete
 const QVector<QColor> SchedulerWidget::PROC_COLORS = {
     QColor("#00e5ff"), QColor("#39ff14"), QColor("#ff9f0a"),
-    QColor("#bf5af2"), QColor("#ff2d55"), QColor("#30d158")
+    QColor("#bf5af2"), QColor("#ff2d55"), QColor("#30d158"),
+    QColor("#ff6b6b"), QColor("#4ecdc4"), QColor("#ffe66d"),
+    QColor("#a8e6cf")
 };
+
+// GanttWidget
+GanttWidget::GanttWidget(QWidget *parent) : QWidget(parent) {
+    setMinimumHeight(120);
+    setAttribute(Qt::WA_OpaquePaintEvent);
+}
+
+void GanttWidget::setGanttData(const QList<GanttEntry> &entries, int totalTicks, int currentTick) {
+    m_entries = entries;
+    m_totalTicks = totalTicks;
+}
 
 schedulerwidget::schedulerwidget() {}
