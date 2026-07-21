29b65d8f026270a83cd4268ab4dd04f754d37a08
Sherwin Dave Osma <sherwinosma1@gmail.com>
2026-07-21 13:32:10 +0800

    GanttWidget::paintEvent

--- schedulerwidget.cpp
+++ schedulerwidget.cpp
@@ -31,6 +31,31 @@ GanttWidget::GanttWidget(QWidget *parent) : QWidget(parent) {
 void GanttWidget::setGanttData(const QList<GanttEntry> &entries, int totalTicks, int currentTick) {
     m_entries = entries;
     m_totalTicks = totalTicks;
+    m_currentTick = currentTick;
+
+    // Compute requried width
+    if (totalTicks > 0) {
+        double pxPerTick = (double)(width() - LEFT_MARGIN - RIGHT_MARGIN) / totalTicks;
+        pxPerTick = qMax(pxPerTick, 18.0);
+        setMinimumWidth(LEFT_MARGIN + (int)(pxPerTick * totalTicks) + RIGHT_MARGIN);
+    }
+
+    update();
+}
+
+void GanttWidget::setCurrentTick(int tick) {
+    m_currentTick = tick;
+    update();
+}
+
+void GanttWidget::resizeEvent(QResizeEvent *event) {
+    QWidget::resizeEvent(event);
+    if (m_totalTicks > 0) setGanttData(m_entries, m_totalTicks, m_currentTick);
+}
+
+void GanttWidget::paintEvent(QPaintEvent *) {
+    QPainter p(this);
+    p.setRenderHint(QPainter::Antialiasing);
 }
 
 schedulerwidget::schedulerwidget() {}
